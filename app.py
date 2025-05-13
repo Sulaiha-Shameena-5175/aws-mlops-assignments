7b5af377d1ce1a4f6faac85f4d564e447c866f72
Sulaiha-Shameena-5175 <sulaiha.shameena@tigeranaltyics.com>
2025-05-13 10:20:44 +0530

    add: app.py, requirements.txt

--- app.py
+++ app.py
@@ -3,14 +3,28 @@ import boto3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/folders")
 def list_files():
-    s3 = boto3.client('s3')
-    response = s3.list_buckets()
 
-    # Output the bucket names
-    print('Existing buckets:')
-    for bucket in response['Buckets']:
-        print(f'  {bucket["Name"]}')
-    return "<p>pass</p>"
+    prefix = 'home/sulaiha.shameena/'
+
+    session = boto3.Session(profile_name='pe-mle-user-role')
+    s3 = session.client('s3')
+
+    list_obj = s3.list_objects_v2(Bucket = 'tiger-mle-pg', Prefix = prefix, Delimiter='/')
+    folders_response = list_obj.get('CommonPrefixes', [])
+    folder_dict = dict()
+
+    for folder in folders_response: 
+        main_folder = folder['Prefix'].replace(prefix,'')
+        file_list = s3.list_objects_v2(Bucket = 'tiger-mle-pg', Prefix = folder['Prefix'])
+        files = (file_list.get('Contents', []))
+        folder_dict[main_folder] = []
+
+        for eachFile in files: 
+            if (eachFile['Key'].endswith('.txt')):
+                sub_folder = eachFile['Key'].replace(folder['Prefix'], '')
+                folder_dict[main_folder].append(sub_folder)
+                
+    return folder_dict
     
